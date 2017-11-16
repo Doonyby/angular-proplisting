48b25c04a6329a1ac38567ca94e7bcae406ffb33
Doonyby <doonyby@gmail.com>
2017-11-16 13:25:19 -0700

    initial commit and generated some new components with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { ListingsComponent } from './component/listings/listings.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AddListingComponent } from './component/add-listing/add-listing.component';
+import { EditListingComponent } from './component/edit-listing/edit-listing.component';
+import { ListingComponent } from './component/listing/listing.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'listings', component:ListingComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    AddListingComponent,
+    EditListingComponent,
+    ListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
